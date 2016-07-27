e1349b1e758cd4747d7f26ed12671f30cd991f88
Elaine Lin <elin1@mit.edu>
2016-07-27 12:43:22 -0400

    Setting up state select

--- app/components/StateSelect/index.js
+++ app/components/StateSelect/index.js
@@ -3,6 +3,19 @@ import React from 'react';
 import styles from './styles.css';
 
 function StateSelect(props) {
+  var options = require('./states.json');
+  console.log(options);
+  // return (
+  //   <div className={classes.select}>
+  //     <Select
+  //     value={props.selectedState}
+  //     options={options}
+  //     placeholder='Select a state'
+  //     onChange={e => props.changeState(e.value)}
+  //     clearable={false}
+  //     />
+  //   </div>
+  // );
   return (
     <div>
         State Select here!
