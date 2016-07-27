688c37712fc32d5ff5588cdfc5842e8652de4e22
Elaine Lin <elin1@mit.edu>
2016-07-27 15:25:07 -0400

    Transferring more check reg field info

--- app/components/CheckRegistrationField/index.js
+++ app/components/CheckRegistrationField/index.js
@@ -12,17 +12,52 @@ import ToggleOption from '../ToggleOption';
 
 function CheckRegistrationField(props) {  // eslint-disable-line react/prefer-stateless-function
 
+  var text = 'TEXT';
+  var select = 'SELECT';
+  var check = 'CHECK';
+
+  // var label = props.field.label;
+  // var fieldType = props.field.type;
+  // var conf = props.field.conf; 
+  // var fieldWidth = props.field.width;
+
+  var parseConfOptions = function(conf) {
+    var splitConf = conf.split(',');
+    var options = [];
+
+    for(var i = 1; i < splitConf.length; i++) {
+      var temp = splitConf[i].split(':');
+      options.push({label: temp[0], value: temp[1]});
+    }
+
+    return options;
+  }
+
+  // var formField;
+  // if (fieldType == text) {
+  //   formField = 
+  //   <input type='text' className='form-control' id={label} placeholder={label}/>
+  // }
+  // if (fieldType == select) {
+  //   formField =     
+  //   <Select
+  //     value='one'
+  //     options={parseConfOptions(conf)}
+  //     placeholder={label}
+  //     onChange={e => console.log(e.value)}
+  //     clearable={false}
+  //   />
+  // }
+
   return (
-    <div>
-      Hi! I'm a check registration field.
+    <div className='form-group col-md-4'>
+      I'm still a check registration field, but with more code! But the code is hidden... spookyyyyy.
     </div>
   );
 }
 
-// Toggle.propTypes = {
-//   onToggle: React.PropTypes.func,
-//   values: React.PropTypes.array,
-//   messages: React.PropTypes.object,
-// };
+// CheckRegistrationField.propTypes = {
+//   field: React.PropTypes.object.isRequired
+// }
 
 export default CheckRegistrationField;
