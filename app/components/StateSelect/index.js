cfc2544be3cef5ebdd63ce67717876289da4ce4f
Elaine Lin <elin1@mit.edu>
2016-08-05 12:22:20 -0400

    Logic for state select

--- app/components/StateSelect/index.js
+++ app/components/StateSelect/index.js
@@ -8,6 +8,25 @@ function StateSelect(props) {
       <option key={`state-${index}`} value={state.code}>{state.label}</option>
     ));
   }
+
+  const API_KEY = 'AIzaSyAMfwLqZ6TriYOFafbR6ty6cKFIjRyIB3w';
+  fetch('https://www.googleapis.com/geolocation/v1/geolocate?key='+API_KEY, { method:'POST' })
+  .then(function(response) {
+    response.json().then(function(data) {
+      var lat = data.location.lat;
+      var lng = data.location.lng;
+      fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lng+'&key='+API_KEY, { method:'POST'})
+      .then(function(response) {
+        response.json().then(function(data) {
+          var address = data.results[0].address_components;
+          var state = address.filter(function(address, i) {
+            return address.types.indexOf('administrative_area_level_1') > -1;
+          });
+          console.log(state[0].long_name);
+        });
+      });
+    });
+  });
   return (
     <div className="form-group">
       <select className={styles.stateSelect} onChange={props.onChange} defaultValue="">
