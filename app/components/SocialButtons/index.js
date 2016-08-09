597a6f9a25fb1b45c85e82d02fafe97cf89f3b0c
Elaine Lin <elin1@mit.edu>
2016-08-09 13:10:04 -0400

    Added basic styling for social buttons

--- app/components/SocialButtons/index.js
+++ app/components/SocialButtons/index.js
@@ -31,19 +31,19 @@ const PinterestIcon = generateShareIcon('pinterest');
 
 function SocialButtons() {
   return (
-    <div className="buttons-container">
+    <div className={styles.buttons}>
       <FacebookShareButton
-            url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAQxxq8RSEVWAQWJEt58Ura_KSXYKShhzhYHy-gI8Xgwn0lxkwfxrpY2mY"
-            title="a cute cat"
-            className="Demo__some-network__share-button">
-            <FacebookIcon
-              size={32}
-              round />
+        url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAQxxq8RSEVWAQWJEt58Ura_KSXYKShhzhYHy-gI8Xgwn0lxkwfxrpY2mY"
+        title="a cute cat"
+        className={styles.social}>
+        <FacebookIcon
+          size={32}
+          round />
       </FacebookShareButton>
       <TwitterShareButton
         url="http://www.tpevent.com/wp-content/uploads/2016/01/cute-cat-images-tpevent-5.jpg"
         title="twitter cute cat"
-        className="Demo__some-network__share-button">
+        className={styles.social}>
         <TwitterIcon
           size={32}
           round />
