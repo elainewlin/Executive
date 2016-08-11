e39e010e2af1c96cecc1f4b10c6e26f57d638322
Elaine Lin <elin1@mit.edu>
2016-08-11 11:54:11 -0400

    Added basic file reading and select tests

--- app/tests/checkRegTests.py
+++ app/tests/checkRegTests.py
@@ -1,15 +1,41 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import os
+# add users.json file to app/tests
+# terminal - cd to Executive directory
+# python app/tests/checkRegTests.py
 
-chromedriver = os.getcwd()+"/chromedriver"
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+import json
+
+directory = os.getcwd()+"/app/tests/"
+# set up test data 
+with open(directory+"users.json") as data_file:    
+  data = json.load(data_file)
+allStates = data.keys()
+
+'''
+# set up driver 
+chromedriver = directory+"chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
-browser = webdriver.Chrome(chromedriver)
-browser.get("http://www.python.org")
-assert "Python" in browser.title
-elem = browser.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in browser.page_source
-browser.close()
+driver = webdriver.Chrome(chromedriver)
+driver.get("localhost:3000")
+
+# wait for the API call to load the state select
+WebDriverWait(driver, 10).until(
+  EC.presence_of_element_located((By.CSS_SELECTOR, "#stateSelect > option[value='MA']"))
+)
+stateSelect = Select(driver.find_element_by_id('stateSelect'))
+'''
+
+print data["MA"]
+
+def testState(state):
+  stateSelect.select_by_value(state)
+  print state
+
+'''
+driver.close()
+'''
