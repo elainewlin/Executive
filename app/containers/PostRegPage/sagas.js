3a0be652f9b888ba9767ca031c5483efa419d040
Elaine Lin <elin1@mit.edu>
2016-08-17 16:48:50 -0400

    Styled email form

--- app/containers/PostRegPage/sagas.js
+++ app/containers/PostRegPage/sagas.js
@@ -10,6 +10,7 @@ export function* submitEmail() {
   const email = yield select(selectors.selectEmail());
   const state = yield select(selectors.selectState());
   const registered = yield select(selectors.selectRegistered());
+
   const emailResult = yield call(
   request,
   c.FETCH_MAILCHIMP_URL+email['email']+"&merge_vars[state]="+state+"&merge_vars[registered]="+registered,
