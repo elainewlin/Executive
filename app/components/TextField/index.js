3b18e72726a3e54e5c9694a024462edee24ca526
Elaine Lin <elin1@mit.edu>
2016-08-23 14:20:55 -0400

    Basic form validation

--- app/components/TextField/index.js
+++ app/components/TextField/index.js
@@ -10,7 +10,7 @@ import { Field } from 'redux-form';
 function TextField(props) {
   return (
     <div className={`form-group col-xs-${props.width}`}>
-      <Field type="text" name={props.name} placeholder={props.label} id={props.label} component="input" />
+      <Field type="text" name={props.name} placeholder={props.label} id={props.label} component="input" required={props.required}/>
     </div>
   );
 }
@@ -19,6 +19,7 @@ TextField.propTypes = {
   name: React.PropTypes.string,
   label: React.PropTypes.string,
   width: React.PropTypes.number,
+  required: React.PropTypes.bool,
 };
 
 export default TextField;
