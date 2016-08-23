d6685ea54ba437bdb6b3cf3e01d1206e91013e79
Elaine Lin <elin1@mit.edu>
2016-08-23 15:42:42 -0400

    Added animation to email modal

--- app/components/EmailForm/index.js
+++ app/components/EmailForm/index.js
@@ -10,21 +10,30 @@ import { Field } from 'redux-form';
 import { reduxForm } from 'redux-form/immutable';
 
 function EmailForm(props) {
+  let form;
+  if (props.isSubmitted) {
+    form = (<div>Currently not working. Please try again later.</div>);
+  }
+  else {
+    form = (<form onSubmit={props.submitEmail} className="form-inline">
+      <Field type="text" name="email" className={styles.input} placeholder="Email" component="input" required/>
+      <button type="submit" className={styles.submit}>Submit</button>
+    </form>);
+  }
+
   return (
     <div className={styles.email}>
       <div className={styles.heading}>
         Don't forget to vote! <span className={styles.wrap}>Sign up for reminders.</span>
       </div>
-      <form onSubmit={props.submitEmail} className="form-inline">
-        <Field type="text" name="email" className={styles.input} placeholder="Email" component="input" />
-        <button type="submit" className={styles.submit}>Submit</button>
-      </form>
+      {form}
     </div>
   );
 }
 
 EmailForm.propTypes = {
   submitEmail: React.PropTypes.func,
+  isSubmitted: React.PropTypes.bool,
 };
 
 export default reduxForm({
--- app/containers/PostRegPage/reducer.js
+++ app/containers/PostRegPage/reducer.js
@@ -8,12 +8,15 @@ import { fromJS } from 'immutable';
 import * as c from './constants';
 
 const initialState = fromJS({
+    isSubmitted: false,
 });
 
 function postRegPageReducer(state = initialState, action) {
   switch (action.type) {
     case c.SUBMIT_EMAIL:
-      return state.set('params', action.params);
+      return state
+        .set('params', action.params)
+        .set('isSubmitted', true);
     default:
       return state;
   }
