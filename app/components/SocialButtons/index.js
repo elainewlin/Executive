4ff05a33a7c3a1607c385bdb174fca4695d28c6b
Elaine Lin <elin1@mit.edu>
2016-08-26 14:06:44 -0400

    Added social buttons

--- app/components/SocialButtons/index.js
+++ app/components/SocialButtons/index.js
@@ -19,7 +19,7 @@ import React from 'react';
 import styles from './styles.scss';
 
 const FacebookIcon = generateShareIcon('facebook');
-// const TwitterIcon = generateShareIcon('twitter');
+const TwitterIcon = generateShareIcon('twitter');
 
 function SocialButtons() {
   return (
@@ -33,14 +33,14 @@ function SocialButtons() {
           title="VoteMate - Let's Vote. Together."
           className={styles.social}
         >
-
+        <FacebookIcon size={32} round />
         </FacebookShareButton>
         <TwitterShareButton
           url="votemate.us"
           title="Are you registered to vote?"
           className={styles.social}
         >
-  
+        <TwitterIcon size={32} round />
         </TwitterShareButton>
       </div>
     </footer>
